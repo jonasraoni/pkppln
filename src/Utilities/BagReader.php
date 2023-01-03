93f432e8a1c0714c92082eeee54a9362fbe89e7c
Jonas Raoni Soares da Silva <github@raoni.org>
2023-01-03 13:30:32 +0300

    #21 Added method to load a compressed bag in a specific directory

--- src/Utilities/BagReader.php
+++ src/Utilities/BagReader.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 
 namespace App\Utilities;
 
+use Symfony\Component\Filesystem\Filesystem;
 use Throwable;
 use whikloj\BagItTools\Bag;
 use whikloj\BagItTools\BagItException;
@@ -20,12 +21,33 @@ use whikloj\BagItTools\BagItException;
 class BagReader
 {
     /**
+     * Decompresses a bag and retrieves it.
+     *
+     * @throws BagItException&Throwable
+     */
+    public function readCompressedBag(string $path, string $cachePath): Bag
+    {
+        $bag = static::readBag($path);
+        $cachePath = realpath($cachePath);
+        $path = realpath($bag->getBagRoot());
+        // If the bag is in a different path, it was probably temporarily decompressed to another path
+        if ($path && $cachePath && $path !== $cachePath) {
+            $fs = new Filesystem();
+            $fs->rename($path, $cachePath, true);
+            $bag = static::readBag($cachePath);
+        }
+
+        return $bag;
+    }
+
+        /**
      * Read a bag from the file system.
      *
      * @throws BagItException&Throwable
      */
     public function readBag(string $path): Bag
     {
-        return Bag::load($path);
+        $bag = Bag::load($path);
+        return $bag;
     }
 }
