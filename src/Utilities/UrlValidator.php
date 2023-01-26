f5ac58b35c3f5fc9cb6a9ec88f1bcd25109ae41a
Jonas Raoni Soares da Silva <github@raoni.org>
2023-01-26 22:13:34 +0300

    #3 Added URL validator

---
+++ src/Utilities/UrlValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) 2020 Michael Joyce <mjoyce@sfu.ca>
+ * This source file is subject to the GPL v2, bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace App\Utilities;
+
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Validates URLs
+ */
+class UrlValidator
+{
+    /**
+     * Retrieves whether the URL is acceptable by the system
+     */
+    public static function isValid(?string $url, array $forbiddenHosts = []): bool
+    {
+        $host = parse_url((string) $url, PHP_URL_HOST);
+        return !!$host
+            && preg_match('/\./', $host) // Blocks top level domains (localhost, local network name, aliases)
+            && !preg_match('/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/', $host) // Blocks IP addresses
+            && (!count($forbiddenHosts) || !preg_match('/^(?:' . implode('|', array_map(fn (string $host) => preg_quote($host, '/'), $forbiddenHosts)) . ')/i', $host)); // Blocks forbidden hosts
+    }
+}
