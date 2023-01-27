4c94bba37da812c22ba85c18141f1ab5f3a9a157
Jonas Raoni Soares da Silva <github@raoni.org>
2023-01-27 17:22:59 +0300

    #8 Updates tests for the JournalBuilder

--- tests/Services/JournalBuilderTest.php
+++ tests/Services/JournalBuilderTest.php
@@ -16,6 +16,7 @@ use App\Services\JournalBuilder;
 use App\Utilities\Namespaces;
 use App\Tests\TestCase\BaseControllerTestCase;
 use DateTimeInterface;
+use SimpleXMLElement;
 
 /**
  * Description of JournalBuilderTest.
@@ -32,9 +33,9 @@ class JournalBuilderTest extends BaseControllerTestCase
         ];
     }
 
-    private function getXml()
+    private function getXml(): array
     {
-        $data = <<<'ENDXML'
+        $xml = <<<'ENDXML'
 <?xml version="1.0" encoding="utf-8"?>
 <entry xmlns="http://www.w3.org/2005/Atom" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:pkp="http://pkp.sfu.ca/SWORD">
     <email>user@example.com</email>
@@ -59,10 +60,9 @@ class JournalBuilderTest extends BaseControllerTestCase
     </pkp:license>
 </entry>
 ENDXML;
-        $xml = simplexml_load_string($data);
+        $xml = simplexml_load_string($xml);
         Namespaces::registerNamespaces($xml);
-
-        return $xml;
+        return ['B99FE131-48B5-440A-A552-4F1BF2BFDE82', $xml];
     }
 
     public function testInstance(): void
@@ -70,15 +70,21 @@ ENDXML;
         $this->assertInstanceOf(JournalBuilder::class, self::$container->get(JournalBuilder::class));
     }
 
-    public function testResultInstance(): void
+    /**
+     * @dataProvider getXml
+     */
+    public function testResultInstance(string $uid, SimpleXMLElement $xml): void
     {
-        $this->journal = $this->builder->fromXml($this->getXml(), 'B99FE131-48B5-440A-A552-4F1BF2BFDE82');
+        $this->journal = $this->builder->fromXml($xml, $uid);
         $this->assertInstanceOf(Journal::class, $this->journal);
     }
 
-    public function testGetContacted(): void
+    /**
+     * @dataProvider getXml
+     */
+    public function testGetContacted(string $uid, SimpleXMLElement $xml): void
     {
-        $this->journal = $this->builder->fromXml($this->getXml(), 'B99FE131-48B5-440A-A552-4F1BF2BFDE82');
+        $this->journal = $this->builder->fromXml($xml, $uid);
         $this->assertInstanceOf(DateTimeInterface::class, $this->journal->getContacted());
     }
 
@@ -88,27 +94,33 @@ ENDXML;
      * @param mixed $expected
      * @param mixed $method
      */
-    public function testFromXml($expected, $method): void
+    public function testFromXml(string $uid, SimpleXMLElement $xml, array $valueMethodMap): void
     {
-        $this->journal = $this->builder->fromXml($this->getXml(), 'B99FE131-48B5-440A-A552-4F1BF2BFDE82');
-        $this->assertSame($expected, $this->journal->{$method}());
+        $this->journal = $this->builder->fromXml($xml, $uid);
+        foreach ($valueMethodMap as [$expected, $method]) {
+            $this->assertSame($expected, $this->journal->{$method}());
+        }
     }
 
-    public function journalXmlData()
+    public function journalXmlData(): array
     {
-        return [
-            ['B99FE131-48B5-440A-A552-4F1BF2BFDE82', 'getUuid'],
-            [null, 'getOjsVersion'],
-            [null, 'getNotified'],
-            ['Intl J Test', 'getTitle'],
-            ['0000-0000', 'getIssn'],
-            ['http://example.com/ijt', 'getUrl'],
-            ['new', 'getStatus'],
-            [false, 'getTermsAccepted'],
-            ['user@example.com', 'getEmail'],
-            ['Publisher institution', 'getPublisherName'],
-            ['http://publisher.example.com', 'getPublisherUrl'],
-        ];
+        $data = $this->getXml();
+        foreach ($data as &$item) {
+            $item[] = [
+                ['B99FE131-48B5-440A-A552-4F1BF2BFDE82', 'getUuid'],
+                [null, 'getOjsVersion'],
+                [null, 'getNotified'],
+                ['Intl J Test', 'getTitle'],
+                ['0000-0000', 'getIssn'],
+                ['http://example.com/ijt', 'getUrl'],
+                ['new', 'getStatus'],
+                [false, 'getTermsAccepted'],
+                ['user@example.com', 'getEmail'],
+                ['Publisher institution', 'getPublisherName'],
+                ['http://publisher.example.com', 'getPublisherUrl'],
+            ];
+        }
+        return $data;
     }
 
     /**
@@ -117,27 +129,33 @@ ENDXML;
      * @param mixed $expected
      * @param mixed $method
      */
-    public function testFromRequest($expected, $method): void
+    public function testFromRequest(string $uid, SimpleXMLElement $xml, array $valueMethodMap): void
     {
-        $this->journal = $this->builder->fromRequest('B99FE131-48B5-440A-A552-4F1BF2BFDE82', 'http://example.com/journal');
-        $this->assertSame($expected, $this->journal->{$method}());
+        $this->journal = $this->builder->fromRequest($uid, 'http://example.com/journal');
+        foreach ($valueMethodMap as [$expected, $method]) {
+            $this->assertSame($expected, $this->journal->{$method}());
+        }
     }
 
-    public function journalRequestData()
+    public function journalRequestData(): array
     {
-        return [
-            ['B99FE131-48B5-440A-A552-4F1BF2BFDE82', 'getUuid'],
-            [null, 'getOjsVersion'],
-            [null, 'getNotified'],
-            [null, 'getTitle'],
-            [null, 'getIssn'],
-            ['http://example.com/journal', 'getUrl'],
-            ['new', 'getStatus'],
-            [false, 'getTermsAccepted'],
-            ['', 'getEmail'],
-            [null, 'getPublisherName'],
-            [null, 'getPublisherUrl'],
-        ];
+        $data = $this->getXml();
+        foreach ($data as &$item) {
+            $item[] = [
+                ['B99FE131-48B5-440A-A552-4F1BF2BFDE82', 'getUuid'],
+                [null, 'getOjsVersion'],
+                [null, 'getNotified'],
+                [null, 'getTitle'],
+                [null, 'getIssn'],
+                ['http://example.com/journal', 'getUrl'],
+                ['new', 'getStatus'],
+                [false, 'getTermsAccepted'],
+                ['', 'getEmail'],
+                [null, 'getPublisherName'],
+                [null, 'getPublisherUrl'],
+            ];
+        }
+        return $data;
     }
 
     public function testFromRequestExisting(): void
